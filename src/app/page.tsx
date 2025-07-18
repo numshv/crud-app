01b12df3f17c234c3f46ab07db7d9b5d30faeeee
numshv <noumisyifa@gmail.com>
2025-07-18 21:02:36 +0700

    feat: add hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import Hero from "@/components/ui/Hero";
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <Hero/>
     </div>
   );
 }
