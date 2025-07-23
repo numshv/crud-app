b79b88bc6d430abeefc7af2ed479d985acc96773
numshv <noumisyifa@gmail.com>
2025-07-23 19:32:46 +0700

    fix: fix deploy error

--- src/app/plants/[slug]/page.tsx
+++ src/app/plants/[slug]/page.tsx
@@ -1,24 +1,8 @@
-import React, { use } from 'react'
+import React from 'react'
 import PlantCard from './PlantCard'
 import { getPlantById } from '@/actions/plant.action'
-import { title } from 'process';
-import { Description } from '@radix-ui/react-dialog';
 import { stackServerApp } from '@/stack';
 import { SignUp } from '@stackframe/stack';
-
-export async function name({
-  params,
-}: {
-  params: {slug: string};
-}) {
-  const[id] = params.slug.split("--")
-  const plant = await getPlantById(id) 
-
-  return{
-    title: plant ? plant.name : "Plant Details",
-    description: plant ? plant.description : "Plants details description"
-  }
-}
 
 
 async function page({params}: {params: {slug: string}}) {
